71dab006672aa9250abd24c24466170afcd9f67e
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2017-06-04 16:18:19 +0800

    Update tools.sh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -2,12 +2,7 @@
 ### every exit != 0 fails the script
 set -e
 
-echo "Install some common tools for further installation"
-apt-get update 
-apt-get install -y vim wget net-tools
-
-
-
+echo "Install some desktop tools for further installation"
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections 
 echo keyboard-configuration keyboard-configuration/layout select 'English (US)' | debconf-set-selections
 echo keyboard-configuration keyboard-configuration/layoutcode select 'us' | debconf-set-selections 
